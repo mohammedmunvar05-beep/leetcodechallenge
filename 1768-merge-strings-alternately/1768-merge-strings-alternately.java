de574304556e085141eb4ee327b54887fc45e993
Munvar_Md <mohammedmunvar05@gmail.com>
2026-09-21 22:19:20 +0530

    Time: 1 ms (95.67%), Space: 43.1 MB (49.69%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder s=new StringBuilder();
+        int i=0;
+        int j=0;
+        while(i<word1.length()||j<word2.length()){
+            if(i<word1.length()){
+                s.append(word1.charAt(i));
+                i++;
+            }
+            if(j<word2.length()){
+                s.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return s.toString();
+
+    }
+}
